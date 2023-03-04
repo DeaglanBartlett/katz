3577a865b8b34a5a1b583058554792e9d9281e8f
Deaglan Bartlett <deaglanbartlett@admins-MacBook-Pro.local>
2023-03-04 14:41:10 +0100

    Convert variable names in equation file

---
+++ katz/process_equations.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import string
+import csv
+
+def split_by_punctuation(s):
+    pun = string.punctuation.replace('_', '') # allow underscores in variable names
+    pun = pun + ' '
+    where_pun = [i for i in range(len(s)) if s[i] in pun]
+    split_str = [s[:where_pun[0]]]
+    for i in range(len(where_pun)-1):
+        split_str += [s[where_pun[i]]]
+        split_str += [s[where_pun[i]+1:where_pun[i+1]]]
+    split_str += [s[where_pun[-1]]]
+    if where_pun[-1] != len(s) - 1:
+        split_str += [s[where_pun[-1]+1:]]
+    return split_str
+    
+def standardise_file(in_name, out_name):
+
+    df = pd.read_csv(in_name)
+    
+    maxvar = int(df['# variables'].max())
+    if maxvar > 10:
+        raise NotImplementedError(f"Cannot have more than 10 input variables: you have {maxvar}")
+        
+    all_eq = []
+    
+    with open(out_name, 'w') as f:
+        csvwriter = csv.writer(f)
+        
+        csvwriter.writerow(['Filename', 'Number', 'Old Formula', 'New Formula'])
+    
+        for index, row in df.iterrows():
+        
+            if not np.isfinite(row['# variables']):
+                continue
+            eq = row['Formula']
+            
+            # If equation already has variable 'x{i}" then we don't need to replace it
+            # Note: At least Eqs I.18.12, I.18.14 and II.37.1 in AIFeynman has wrong number of vars
+            # The next two lines fix this
+            vars = [row[f'v{i+1}_name'] for i in range(maxvar)]
+            vars = [v for v in vars if isinstance(v, str)]
+                
+            names = [f'x{i}' for i in range(len(vars))]
+            to_change = list(sorted(set(vars) - set(names), key=vars.index))
+            to_sub = list(sorted(set(names) - (set(vars) - set(to_change)), key=names.index))
+            
+            # Must split by punctuation to avoid replacing e.g. "t" in "sqrt" if we have a variable "t"
+            sub_dict = dict(zip(to_change,to_sub))
+            split_eq = split_by_punctuation(eq)
+            for i, s in enumerate(split_eq):
+                if s in to_change: split_eq[i] = sub_dict[s]
+            eq = ''.join(split_eq)
+
+            if row['Number'] == 22:
+                print([row[f'v{i+1}_name'] for i in range(int(row['# variables']))])
+                
+            all_eq.append(eq)
+            
+            csvwriter.writerow([row['Filename'], row['Number'], row['Formula'], eq])
+
+    return
+
+"""
+TO DO
+- Change Feynman equations to identify variables and constants as different?
+- Allow more than 10 input variables
+"""
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import string
 from katz.good_turing import GoodTuring
 from katz.back_off import BackOff
+import katz.process_equations as process_equations
 
 with open('data/romeoandjuliet.txt', 'r') as f:
     data = f.readlines()[75:4494]
@@ -8,15 +9,18 @@ with open('data/romeoandjuliet.txt', 'r') as f:
     data = ' '.join(data)
 data = data.translate(str.maketrans('', '', string.punctuation)).lower().split()
 
-#data = data[:1000]
+#example = 0
+#example = 1
+example = 2
 
-#gt = GoodTuring(data)
-#gt.plot_fit()
-#quit()
-#for word in ['verona', 'shall', 'our']:
-#    k = gt.actual_count(word)
-#    kstar = gt.expected_count(word)
-#    print(word, k, kstar)
+if example == 0:
+    gt = GoodTuring(data)
+    gt.plot_fit()
+
+    for word in ['verona', 'shall', 'our']:
+        k = gt.actual_count(word)
+        kstar = gt.expected_count(word)
+        print(word, k, kstar)
     
 ## Split data into tuples of n
 #w = 'verona'
@@ -31,26 +35,32 @@ data = data.translate(str.maketrans('', '', string.punctuation)).lower().split()
 #        print(i, len(bo.all_gt[i].corpus), bo.all_gt[i].corpus[-4:])
 #quit()
 
-n = 3
-new_data = [tuple(data[i:i+n]) for i in range(len(data)-n+1)]
-bo = BackOff(new_data)
+elif example == 1:
 
-phrase = ('i', 'will')
-seen, unseen = bo.sort_endings(phrase)
-#print(seen)
+    n = 3
+    new_data = [tuple(data[i:i+n]) for i in range(len(data)-n+1)]
+    bo = BackOff(new_data)
 
-for w in ['kiss', 'not', 'romeo', 'shakespeare']:
+    phrase = ('i', 'will')
+    seen, unseen = bo.sort_endings(phrase)
+    #print(seen)
+    print(phrase)
 
-    print('\nSTARTING:', w)
-    new_phrase = phrase + (w,)
-    c = bo.all_gt[len(new_phrase)].actual_count(new_phrase)
-    if c > 0:
-        cstar = bo.all_gt[len(new_phrase)].expected_count(new_phrase)
-    else:
-        cstar = 0
-    pbo = bo.get_pbo(w, phrase)
-    print(w, c, cstar, pbo)
+    for w in ['kiss', 'not', 'romeo', 'shakespeare']:
+
+        print('\nSTARTING:', w)
+        new_phrase = phrase + (w,)
+        c = bo.all_gt[len(new_phrase)].actual_count(new_phrase)
+        if c > 0:
+            cstar = bo.all_gt[len(new_phrase)].expected_count(new_phrase)
+        else:
+            cstar = 0
+        pbo = bo.get_pbo(w, phrase)
+        print(w, c, cstar, pbo)
         
+elif example == 2:
+
+    process_equations.standardise_file('data/FeynmanEquations.csv', 'data/NewFeynman.csv')
     
 """
 TO DO
