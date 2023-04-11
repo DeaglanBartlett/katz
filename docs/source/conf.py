949998c3d8df856265b08b7691df21ea5130cf03
Deaglan Bartlett <deaglan.bartlett@physics.ox.ac.uk>
2023-04-11 09:33:16 +0100

    Add path to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(1, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../katz/'))
+sys.path.insert(0, os.path.abspath('../../scripts/'))
 
 autodoc_mock_imports = ["mpi4py", "astropy"]
 
